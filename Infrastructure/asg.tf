8560ca2cddeee66ff8e01948fcef9638b1687b4a
Marcel <236373202+marcel-erbas@users.noreply.github.com>
2026-01-19 23:30:09 +0100

    scaling: increase asg max size to 4

--- Infrastructure/asg.tf
+++ Infrastructure/asg.tf
@@ -43,7 +43,7 @@ resource "aws_launch_template" "grocery_shop_lt" {
 resource "aws_autoscaling_group" "grocery_shop_asg" {
   name                = "grocery-shop-asg"
   desired_capacity    = 2
-  max_size            = 3
+  max_size            = 4
   min_size            = 1
   
   target_group_arns   = [aws_lb_target_group.grocery_shop_tg.arn]
